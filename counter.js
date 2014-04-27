45b06e512c0313e12b8672683549ebd6f36d41ea
Tom <tommy@daftscience.com>
2014-04-27 01:56:33 -0400

    Fixed IE Bugs

--- counter.js
+++ counter.js
@@ -28,6 +28,19 @@ jQuery(document).keydown(function (e) {
 
 });
 
+//This functions is here because IE thinks it only needs to show the fa-awesome 
+//icons the first time the page is loaded. 
+$(function () {
+var head = document.getElementsByTagName('head')[0],
+	style = document.createElement('style');
+style.type = 'text/css';
+style.styleSheet.cssText = ':before,:after{content:none !important';
+head.appendChild(style);
+setTimeout(function(){
+head.removeChild(style);
+}, 0);
+});
+
 //resize header on scroll
 $(document).on("scroll", function () {
     if ($(document).scrollTop() > 100) {
