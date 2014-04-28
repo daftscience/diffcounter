ba25ea0b5e9581388df4d8130099a8e3581b67ee
daftscience <tommy@daftscience.com>
2014-04-28 15:14:30 -0400

    cleaning up a bit

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -4,8 +4,6 @@ var countTo = 100;
 var KeyID;
 var diffName;
 var nag = false;
-
-
 var seedCells = {
     Neutrophils: "neut",
     "Bands Cells": "band",
@@ -22,92 +20,21 @@ var seedCells = {
     Megakaryocytes: "mega",
     "Plasma Cells": "plasma"
 };
-function toggleDisableX3(){
-//window.alert("in toggleDisableX3");
- toggleDisable();
- toggleDisable();
- toggleDisable();
-}
-
-jQuery(document).keydown(function (e) {
-    KeyCheck(e);
-    $('#debug').html("Key pressed: " + e.keyCode + "<br /> Editing =" + editing + "<br /> Adding = " + adding + "<br /> Nagging = "+ nag);
-
-});
-
-//This functions is here because IE thinks it only needs to show the fa-awesome 
-//icons the first time the page is loaded. 
-$(function () {
-var head = document.getElementsByTagName('head')[0],
-	style = document.createElement('style');
-style.type = 'text/css';
-style.styleSheet.cssText = ':before,:after{content:none !important';
-head.appendChild(style);
-setTimeout(function(){
-head.removeChild(style);
-}, 0);
-});
-
-//resize header on scroll
-$(document).on("scroll", function () {
-    if ($(document).scrollTop() > 100) {
-        $("header").addClass("shrink");
-    } else {
-        $("header").removeClass("shrink");
-    }
-});
-
-
-$('.btn-group[data-input]').each(function () {
-    var hidden = $('[name="' + $(this).data('input') + '"]');
-    $(this).on('click', '.btn', function () {
-        hidden.val($(this).val());
-    }).find('.btn').each(function () {
-        $(this).toggleClass('active', $(this).val() == hidden.val())
-    });
-});
-
-
-function seedHTML(whichTable) {
-
-    if (whichTable === "cells") {
-        for (var cell in seedCells) {
-            if (seedCells.hasOwnProperty(cell)) {
-                document.write("<tr><td>");
-
-                if (!hasIE_ughhh) {
-                    document.write("<input type='number' id='");
-                } else {
-                    document.write("<input type='text' id='");
-                }
-
-                document.write(seedCells[cell]);
-                document.write("' value='' class='count' readOnly/>");
-                document.write("</td><td>");
-                document.write(cell + "</td></tr>");
-            }
-        }
-    } else {
-        for (var cell in seedCells) {
-            if (seedCells.hasOwnProperty(cell)) {
-                document.write("<div class='progress progress-striped'>");
-                document.write("<div id='" + seedCells[cell] + "Prog'");
-                document.write("class='progress progress-bar progress-bar-theme text-right' role='progressbar'");
-                document.write("aria-valuenow='100' aria-valuemin='0' aria-valuemax='100'");
-                document.write("style='width: 0%; min-width='100px;''>");
-                document.write("<span class='progress-text text-right'>0%</span></div></div>");
-            }
-        }
-
-    }
-}
-
-
-
-//This adds the onclick function to all of the buttons
+var whichIE;
 window.onload = function () {
+    
+//attach onlcicks    
+    $("#subtractBTN").click(function(){ toggleSubtract('toggle'); });
+    $("#resetBTN").click(function(){ resetForm(); });
+    $("#editBTN").click(function(){ toggleDisable(); });
+    $("#printBTN").click(function(){ window.print(); });
+    
     //    document.getElementsByClassName('count')
-
+    // IE7 support for querySelectorAll in 226 bytes... It's a little slow but better than a 20kb solution when you need something cross platform and lightweight.
+if(whichIE == 7){
+    window.alert("omg y u still using ie 7?");
+    (function(d,s){d=document,s=d.createStyleSheet();d.querySelectorAll=function(r,c,i,j,a){a=d.all,c=[],r=r.replace(/\[for\b/gi,'[htmlFor').split(',');for(i=r.length;i--;){s.addRule(r[i],'k:v');for(j=a.length;j--;)a[j].currentStyle.k&&c.push(a[j]);s.removeRule(0)}return c}})()
+}
     var anchors = document.querySelectorAll('.count');
     for (var i = 0; i < anchors.length; i++) {
         var anchor = anchors[i];
@@ -121,52 +48,37 @@ window.onload = function () {
             this.style.backgroundColor = ''
         };
     }
-    diffName = document.getElementById('diffName');
+    diffName = document.getElementById("diffName");
     resetForm();
 }
+jQuery(document).keydown(function (e) {
+    KeyCheck(e);
+    $('#debug').html("Key pressed: " + e.keyCode + "<br /> Editing =" + editing + "<br /> Adding = " + adding + "<br /> Nagging = " + nag);
+});
 
-function changeCount(countSelected) {
-    countTo = countSelected.value;
+//This functions is here because IE thinks it only needs to show the fa-awesome 
+//icons the first time the page is loaded.  I don't think its being called? look into this
+function refresh() {
+    $(function () {
+        var head = document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
+        style.type = 'text/css';
+        style.styleSheet.cssText = ':before,:after{content:none !important';
+        head.appendChild(style);
+        setTimeout(function () {
+            head.removeChild(style);
+        }, 0);
+    });
 }
 
-if (window.addEventListener) {
-    // create the keys and konami variables
-    var keys = [],
-    konami = "38,38,40,40,37,39,37,39,66,65";
-    debug = "38,38,38,40,40,40";
-    jillian = "74,73,76,76,73,65,78";
-    kitty = "75,73,84,84,89";
-
-    // bind the keydown event to the Konami function
-    window.addEventListener("keydown", function (e) {
-        // push the keycode to the 'keys' array
-
-        keys.push(e.keyCode);
-
-        // and check to see if the user has entered the Konami code
-        if (keys.toString().indexOf(konami) >= 0) {
-            // do something such as:
-            document.getElementById('tardis').play();
-            $('#wylajb').show();
-            // and finally clean up the keys array
-            keys = [];
-        }
-        if (keys.toString().indexOf(debug) >= 0) {
-            // do something such as:
-            $('#debug').toggle();
-            // and finally clean up the keys array
-            keys = [];
-        }
-
-        if (keys.toString().indexOf(kitty) >= 0) {
-            // do something such as:
-            window.location.href = "https://www.youtube.com/watch?v=Dt4zvJNXbdI";
-            // and finally clean up the keys array
-            keys = [];
-        }
-
-    }, true);
-} //cleaned
+//resize header on scroll
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 100) {
+        $("header").addClass("shrink");
+    } else {
+        $("header").removeClass("shrink");
+    }
+});
 
 function resetForm() {
     //Returns counter to addition mode. 
@@ -188,13 +100,12 @@ function resetForm() {
     for (var i = 0; i < inputs.length; i++) {
         inputs[i].readOnly = "readonly";
     }
-    $("#lockButton").removeClass("fa-unlock");
-    $("#lockButton").addClass("fa-lock");
     recalc();
     editing = false;
+    toggleLockIcon();
     changeAlert('ready');
-    disabled = true;
-    diffName.value = '';
+    //	  disabled = "true";
+    //diffName.value = '';
     diffName.disabled = true;
 
     document.getElementById('c100').click();
@@ -203,7 +114,6 @@ function resetForm() {
 
 
 } //CLeaned..kinda?
-
 //Changes the status on the top.
 function changeAlert(activateAlert) {
     //    document.getElementsByClassName('changeable')
@@ -214,41 +124,92 @@ function changeAlert(activateAlert) {
     jQuery('#' + activateAlert).show();
 } //cleaned
 
-function toggleDisable(option) {
-//window.alert("in toggleDisable");
+function toggleLockIcon() {
+//    window.alert("toggle lock icons");
+    var list = $(document.querySelectorAll('.lockIcons'));
+    $.each(list, function (index, data) {
+        jQuery(this).removeClass("fa-lock");
+        jQuery(this).removeClass("fa-unlock");
+        if (editing == false) {
+            //			window.alert("locking soon edititing = "+ editing);
+            jQuery(this).addClass("fa-lock");
+            //			window.alert("should be locked");
+        } else {
+            //		  window.alert("unlocking soon");
+            jQuery(this).addClass("fa-unlock");
+            //			window.alert("should be unlocked");
+        }
+    });
+
+}
+
+function toggleSubtract(mode) {
+    switch (mode) {
+    case 'toggle':
+        if (!adding) {
+            changeAlert('ready');
+        } else {
+            changeAlert('subtract');
+        }
+        adding = !adding;
+        break;
+    case 'subtract':
+        adding = false;
+        changeAlert('subtract');
+        recalc(); //just incase, to make sure nothing funny happened.
+        //Also this is a quick way to clean up the zeros
+        break;
+    case 'add':
+        adding = true;
+        changeAlert('ready');
+        recalc(); //just incase, to make sure nothing funny happened.
+        //Also this is a quick way to clean up the zeros
+        break;
+    default:
+        adding = true;
+    }
+    $('#subtractIcon').addClass("fa-minus");
+    $('#subtractIcon').addClass("fa-plus");
+    $('#subtractBTN').addClass("btn-warning");
+    $('#subtractBTN').addClass("btn-success");
+
+    if (!adding) {
+        //adding opposite because button will enable the other event, not the current state.
+        $('#subtractIcon').removeClass("fa-minus");
+        $('#subtractBTN').removeClass("btn-warning");
+    } else {
+        $('#subtractIcon').removeClass("fa-plus");
+        $('#subtractBTN').removeClass("btn-success");
+    }
+
+}
+
+function toggleDisable() {
     //switch back to addition mode.
     document.getElementById('safeFocus').focus();
-    $("#lockButton").removeClass("fa-lock");
-    $("#lockButton").removeClass("fa-unlock");
-
-    
     var ae = document.activeElement.nodeName.toLowerCase();
     try {
-    // Support: IE9+
-    // If the <body> is blurred, IE will switch windows, see #9520
-    if ( document.activeElement && ae !== "body" && ae !== "html") {
-        // Blur any element that currently has focus, see #4261
-        $( document.activeElement ).blur();
+        // Support: IE9+
+        // If the <body> is blurred, IE will switch windows, see #9520
+        if (document.activeElement && ae !== "body" && ae !== "html") {
+            // Blur any element that currently has focus, see #4261
+            $(document.activeElement).blur();
+        }
+    } catch (error) { window.alert("error");    
     }
-} catch ( error ) {}
-    
-    
-    
-    
     
     toggleSubtract('addEdit');
+
     //check if the disabled flag is set and toggle it. Change content to refect the new mode.
     if ($('#edit').is(':visible')) {
         changeAlert('ready');
-        $("#lockButton").addClass("fa-lock");
-
     } else {
         changeAlert('edit');
-        $("#lockButton").addClass("fa-unlock");
     }
     diffName.disabled = editing;
     editing = !editing;
-
+    //window.alert("icons should toggle here");
+    toggleLockIcon();
     //Runs through all the variables and toggles whether or not they are 
     //enabled. This will also change the appearence of them to reflect the
     //current mode. 
@@ -274,9 +235,9 @@ function increment(cell) {
         }
     }
 
-//    if (!hasIE_ughhh) {
-        cell.focus();
-//    }
+    //    if (!hasIE_ughhh) {
+    cell.focus();
+    //    }
     recalc();
 } //inda cleaned
 
@@ -302,7 +263,6 @@ function KeyCheck(evt) {
             return;
         }
     }
-    
     //document.get(Element
     var cell = '';
     //Picks the Key
@@ -394,9 +354,9 @@ function KeyCheck(evt) {
         evt.preventDefault();
         return;
     case 69: //edit mode
-//For some reason internet explorer will display the page properly if I do this times three. #whatTheFuck!
-	toggleDisableX3();
-	evt.preventDefault();
+        //For some reason internet explorer will display the page properly if I do this times three. #whatTheFuck!
+        toggleDisable();
+        evt.preventDefault();
         return;
     case 82: //reset
         resetForm();
@@ -429,36 +389,6 @@ function KeyCheck(evt) {
     }
 }
 
-function toggleSubtract(mode) {
-    switch (mode) {
-    case 'toggle':
-        if (!adding) {
-            changeAlert('ready');
-        } else {
-            changeAlert('subtract');
-        }
-        adding = !adding;
-        break;
-    case 'subtract':
-        adding = false;
-        changeAlert('subtract');
-        recalc(); //just incase, to make sure nothing funny happened.
-        //Also this is a quick way to clean up the zeros
-        break;
-    case 'add':
-        adding = true;
-        changeAlert('ready');
-        recalc(); //just incase, to make sure nothing funny happened.
-        //Also this is a quick way to clean up the zeros
-        break;
-    default:
-        adding = "true";
-        return;
-    }
-}
-
-
-
 
 function recalc() {
     var tot = 0;
@@ -485,25 +415,90 @@ function recalc() {
 
     //Check if finished with diff
     //    document.getElementById('maxNum').value
- 
-    if (tot < countTo){
-	nag = false;
-	}
-    if (tot > countTo && !nag){
+
+    if (tot < countTo) {
+        nag = false;
+    }
+    if (tot > countTo && !nag) {
         window.alert("someone's an overachiever, you've somehow counted more than " + countTo + " cells!");
-    	nag = true;
-	}
-    if (tot == countTo && !nag ) {
-        if(!editing && !nag){
-          changeAlert('done');
-          window.alert('way to go!');
-	  nag = true;
+        nag = true;
+    }
+    if (tot == countTo && !nag) {
+        if (!editing && !nag) {
+            changeAlert('done');
+            window.alert('way to go!');
+            nag = true;
         }
     }
 }
+if (window.addEventListener) {
+    // create the keys and konami variables
+    var keys = [],
+        konami = "38,38,40,40,37,39,37,39,66,65",
+        debug = "38,38,38,40,40,40",
+        jillian = "74,73,76,76,73,65,78",
+        kitty = "75,73,84,84,89";
 
+    // bind the keydown event to the Konami function
+    window.addEventListener("keydown", function (e) {
+        // push the keycode to the 'keys' array
+        keys.push(e.keyCode);
+        // and check to see if the user has entered the Konami code
+        if (keys.toString().indexOf(konami) >= 0) {
+            // do something such as:
+            document.getElementById('tardis').play();
+            $('#wylajb').show();
+            // and finally clean up the keys array
+            keys = [];
+        }
+        if (keys.toString().indexOf(debug) >= 0) {
+            // do something such as:
+            $('#debug').toggle();
+            // and finally clean up the keys array
+            keys = [];
+        }
+        if (keys.toString().indexOf(kitty) >= 0) {
+            // do something such as:
+            window.location.href = "https://www.youtube.com/watch?v=Dt4zvJNXbdI";
+            // and finally clean up the keys array
+            keys = [];
+        }
 
+    }, true);
+} //cleaned
+function seedHTML(whichTable) {
 
+    if (whichTable === "cells") {
+        for (var cell in seedCells) {
+            if (seedCells.hasOwnProperty(cell)) {
+                document.write("<tr><td>");
+
+                if (!hasIE_ughhh) {
+                    document.write("<input type='number' id='");
+                } else {
+                    document.write("<input type='text' id='");
+                }
+
+                document.write(seedCells[cell]);
+                document.write("' value='' class='count' readOnly/>");
+                document.write("</td><td>");
+                document.write(cell + "</td></tr>");
+            }
+        }
+    } else {
+        for (var cell in seedCells) {
+            if (seedCells.hasOwnProperty(cell)) {
+                document.write("<div class='progress progress-striped'>");
+                document.write("<div id='" + seedCells[cell] + "Prog'");
+                document.write("class='progress progress-bar progress-bar-theme text-right' role='progressbar'");
+                document.write("aria-valuenow='100' aria-valuemin='0' aria-valuemax='100'");
+                document.write("style='width: 0%; min-width='100px;''>");
+                document.write("<span class='progress-text text-right'>0%</span></div></div>");
+            }
+        }
+
+    }
+}
 function normalize() {
     var runningTotal = document.getElementById('total').innerHTML;
     var inputs = document.getElementsByTagName("INPUT");
@@ -539,30 +534,8 @@ function normalize() {
         document.getElementById('megaProg').children[0].innerHTML = parseInt((document.getElementById('mega').value / tot) * 100) + '%';
     }
 
-    //fixRoundingError();
+}
+function changeCount(countSelected) {
+    countTo = countSelected.value;
 }
 
-function fixRoundingError() {
-    var totalNorm = document.getElementById('totalNorm').value;
-    var difference = (100 - totalNorm);
-    while (total < 100) {
-        var largestRemainder = 0;
-        var inputs = document.getElementsByClass('count');
-
-
-        for (var i = 0; i < inputs.length; i++) {
-            if (inputs[i].value > (inputs[largestRemainder].value)) {
-                largestRemainder = i;
-            }
-        }
-
-    }
-
-}
-
-function isEven(value) {
-    if (value % 2 === 0)
-        return true;
-    else
-        return false;
-}
