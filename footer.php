3f4b43b59a843791fadbb5b9c88a12ec5a746951
Tom <tom@Daftscience.(none)>
2014-04-29 08:01:27 -0400

    yeah

---
+++ footer.php
@@ -0,0 +1,48 @@
+        <!-- +++++ Footer Section +++++ -->
+        <div id="footer">
+            <div class="container">
+                <div class="centered">
+                    <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">
+                        <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc/4.0/88x31.png" />
+                    </a>
+                    <br />
+                    <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/StillImage" property="dct:title" rel="dct:type">Diffcounter</span>by
+                    <a xmlns:cc="http://creativecommons.org/ns#" href="daftscience.com/diffcounter" property="cc:attributionName" rel="cc:attributionURL">Daftscience</a>is licensed under a
+                    <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
+                    <br />Based on a work at
+                    <a xmlns:dct="http://purl.org/dc/terms/" href="daftscience.com/counter" rel="dct:source">daftscience.com/counter</a>.
+                </div>
+            </div>
+        </div>
+        <a id="toTop" href="#">
+            <div class="to-top"></div>
+        </a>
+        <!-- Bootstrap core JavaScript================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+				<script src="assets/js/placeholders.min.js"></script>
+    </div>
+    <!--      non printable-->
+
+
+
+    <div class="footer">
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://daftscience.com/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="http://daftscience.com/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+    </div>
+  </body>
+</html>
